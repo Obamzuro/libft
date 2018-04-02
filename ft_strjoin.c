b67d6a5c10f467c8a2ba09e391df18cfcb681239
Oleg BAMZUROV <obamzuro@e1r7p17.unit.ua>
2018-04-03 00:09:12 +0300

    strjoin with NULL exceptions (and strlen)

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: obamzuro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/24 22:22:33 by obamzuro          #+#    #+#             */
-/*   Updated: 2018/04/02 14:43:11 by obamzuro         ###   ########.fr       */
+/*   Updated: 2018/04/03 00:08:48 by obamzuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	char	*a;
 	size_t	i;
 
-	if (!s1 || !s2)
-		return (NULL);
 	i = ft_strlen(s1) + ft_strlen(s2) + 1;
 	a = ft_strnew(i);
+	printf("la\n");
 	if (!a)
 		return (NULL);
-	ft_strlcat(a, s1, i);
-	ft_strlcat(a, s2, i);
-	a[i] = 0;
+	if (s1)
+		ft_strlcat(a, s1, i);
+	if (s2)
+		ft_strlcat(a, s2, i);
 	return (a);
 }
