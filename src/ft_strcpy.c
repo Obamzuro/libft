cb59d43266f7ece5abe9d60140c919447af78c0a
Oleg BAMZUROV <obamzuro@e3r1p10.unit.ua>
2018-06-15 12:54:57 +0300

    -g

--- src/ft_strcpy.c
+++ src/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: obamzuro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/21 20:24:09 by obamzuro          #+#    #+#             */
-/*   Updated: 2018/03/26 18:03:51 by obamzuro         ###   ########.fr       */
+/*   Updated: 2018/06/14 15:28:54 by obamzuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@ char	*ft_strcpy(char *dst, const char *src)
 {
 	char *dstc;
 
+	if (!dst || !src)
+		return (0);
 	dstc = dst;
 	while (*src)
 		*dstc++ = *src++;
