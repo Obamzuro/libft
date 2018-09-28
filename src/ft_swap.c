4020d261f81b66630539f39160479e939f0612bd
obamzuro <oleg.bamzurov@gmail.com>
2018-09-28 11:42:10 +0300

    conflicts

--- src/ft_swap.c
+++ src/ft_swap.c
@@ -6,11 +6,7 @@
 /*   By: obamzuro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/09 16:36:46 by obamzuro          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2018/09/27 17:01:38 by obamzuro         ###   ########.fr       */
-=======
-/*   Updated: 2018/09/24 14:34:51 by obamzuro         ###   ########.fr       */
->>>>>>> d768ecb37af9914cab8881a67e02d2db4997fc4c
+/*   Updated: 2018/09/28 11:41:41 by obamzuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +25,6 @@ void	ft_iswap(int *a, int *b)
 {
 	int	temp;
 
-<<<<<<< HEAD
 	temp = *a;
 	*a = *b;
 	*b = temp;
@@ -39,8 +34,6 @@ void	ft_dswap(double *a, double *b)
 {
 	double	temp;
 
-=======
->>>>>>> d768ecb37af9914cab8881a67e02d2db4997fc4c
 	temp = *a;
 	*a = *b;
 	*b = temp;
